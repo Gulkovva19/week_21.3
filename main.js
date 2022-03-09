f66417d87914ebaed541c793c9458d7965126b11
Gulkovva19 <gulkova.sofya@yandex.ru>
2022-03-09 12:01:51 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+let btn = document.getElementById("btn");
+
+btn.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    fetch("https://httpbin.org/post",
+        {
+            method: 'POST',
+            body: new FormData(form),
+            headers: { "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8" },
+        },
+    )
+        .then(response => response.json())
+        .catch(error => console.log(error));
+})
